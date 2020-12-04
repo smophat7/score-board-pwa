3aca54440351e08e92764f417e14e68b349b55f0
Spencer Moffat <spencer.g.moffat@gmail.com>
2020-12-03 23:38:15 -0600

    Group table loading spinner (@saeris/vue-spinners

--- front-end/src/views/Group/Group.vue
+++ front-end/src/views/Group/Group.vue
@@ -32,20 +32,24 @@
     </v-dialog>
 
     <!-- Table and Search -->
+    <div v-if="loadingMembers" class="d-flex loader-container">
+      <RingLoader class="my-auto mx-auto" :loading="loadingMembers" color="#3949ab" :size=200 />
+    </div>
+    <div v-if="!loadingMembers">
+      <v-card-title>
+        <v-text-field v-model="search" prepend-inner-icon="mdi-magnify" clearable label="Search" single-line hide-details></v-text-field>
+      </v-card-title>
 
-    <v-card-title>
-      <v-text-field v-model="search" prepend-inner-icon="mdi-magnify" clearable label="Search" single-line hide-details></v-text-field>
-    </v-card-title>
-
-    <!-- mobile-breakpoint="" eliminates the mobile table view option as I'm doing custom column visibility instead -->
-    <v-data-table
-      :headers="computedHeaders"
-      :items="members"
-      :search="search"
-      mobile-breakpoint=""
-      @click:row="handleDetailClick"
-    >
-    </v-data-table>
+      <!-- mobile-breakpoint="" eliminates the mobile table view option as I'm doing custom column visibility instead -->
+      <v-data-table
+        :headers="computedHeaders"
+        :items="members"
+        :search="search"
+        mobile-breakpoint=""
+        @click:row="handleDetailClick"
+      >
+      </v-data-table>
+    </div>
 
 
     <!-- Dialog/Modal with additional information -->
@@ -68,17 +72,19 @@ import AddNewMember from "@/components/AddNewMember.vue";
 // import HistoryFunctions from "@/mixins/HistoryFunctions.js";
 
 import axios from "axios";
+import { RingLoader } from "@saeris/vue-spinners";
 
 export default {
   name: "Group",
   components: {
     MemberDetails,
     AddNewMember,
+    RingLoader,
   },
-  // mixins: [HistoryFunctions],
   data() {
     return {
       members: [],
+      loadingMembers: false,
       search: "",
       detailDialog: false,
       addNewMemberDialog: false,
@@ -102,14 +108,6 @@ export default {
     ifGroupDataChanged() {
       return this.$store.state.ifGroupChanged;
     }
-    // members() {
-    //   return this.$root.$data.members.map(person => {
-    //     return {
-    //       ...person,
-    //       readableDate: this.readableDate(person.dateJoined),
-    //     }
-    //   });
-    // }
   },
   watch: {
     ifGroupDataChanged() {
@@ -121,6 +119,7 @@ export default {
   },
   methods: {
     async getMembers() {
+      this.loadingMembers = true;
       let url = "http://localhost:3000/members";
       try {
         let response = await axios.get(url);
@@ -131,6 +130,7 @@ export default {
             readableDate: this.readableDate(member.dateJoined),
           }
         });
+        this.loadingMembers = false;
       }
       catch (error) {
         console.log(error)
@@ -155,6 +155,10 @@ export default {
 </script>
 
 <style scoped>
+.loader-container {
+  height: calc(100vh - 208px);
+}
+
 @media (max-width: 600px){
   .mobile-invisible-column {
     display: none !important;
