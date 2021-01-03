8610dc0d25c3954c5d8725a761937f0d9300216e
Spencer M <66281828+smophat7@users.noreply.github.com>
2021-01-03 11:06:50 -0600

    Update authentication.js

--- back-end/middleware/authentication.js
+++ back-end/middleware/authentication.js
@@ -1,13 +1,13 @@
 var admin = require('firebase-admin');
 
 // Middleware for validating user tokens. Must be validated before
-// we can serve any account-sensitive data
+// we can serve any account-sensitive data or make changes 
 
 const getAuthToken = (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-    // console.log("Inside if in getAuthToken, req.authToken: " + req.authToken);
-    // console.log("req.body: " + req.body);
-    // console.log("headers.authorization: " + req.headers.authorization)
+    console.log("Inside if in getAuthToken, req.headers: " + req.headers);
+    console.log("req.body: " + req.body);
+    console.log("headers.authorization: " + req.headers.authorization)
     req.authToken = req.headers.authorization.split(' ')[1];
   }
   else {
@@ -28,7 +28,8 @@ const checkIfAuthenticated = (req, res, next) => {
     }
     catch (error) {
       console.log("authentication error: " + error);
-      return res.status(401).send({ error: 'You are not authorized to make this request.' });
+      // return res.status(401).send({ error: 'You are not authorized to make this request.' });
+      return res.status(401).send({ error });
     }
   });
 };
